1854f92d5cd70c75e8d428b1124339901480649f
“deuro” <hero14yki1986@gmail.com>
2021-07-10 17:15:59 +0000

    Finish top-page

---
+++ app/helpers/static_pages_helper.rb
@@ -0,0 +1,13 @@
+module StaticPagesHelper
+  
+
+  def full_title(page_name = "")
+    base_title = "AttendanceApp"
+    if page_name.empty?
+      base_title
+    else
+      page_name + " | " + base_title
+    end
+  end
+end
+
